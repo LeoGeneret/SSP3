857d787270d957384b14c0595fcee1173a11b5a3
paul <paul.troadec@hetic.net>
2020-01-30 14:57:41 +0100

    add delete/create/update to hotel, visiteur, voiture routes

--- api/database/models/Visiteur.js
+++ api/database/models/Visiteur.js
@@ -71,5 +71,130 @@ module.exports = (sequelize, DataTypes) => {
         return results
     }
 
+    Visiteur.deleteVisiteur = async (visiteurId = null) => {
+        
+        if(visiteurId === null) {
+            return {
+                error:{
+                    message: "BAD REQUEST - you must specify visiteurId",
+                    code: 400
+                },
+                data: null,
+                status: 400
+            }
+        }
+
+        let results = {
+            error: false,
+            status: 200,
+            data: null
+        }
+
+        let visiteur = null
+
+        try {
+            visiteur = await Visiteur.destroy({
+                where: {
+                    id: visiteurId
+                }
+            })
+            if(visiteur === 0){
+                results.error = {
+                    code: 404,
+                    message: "NOT FOUND - no visiteur found"
+                }
+                results.status = 404
+            } else {
+                results.status = 202
+                results.data = "deleted"
+            }
+        } catch (DeleteVisiteurError) {
+            console.error({DeleteVisiteurError})
+            results.error = {
+                code: 502,
+                message: "BAD GATEWAY - error on deleting ressource"
+            }
+            results.status = 502
+        }
+
+        return results
+    }
+
+    Visiteur.createVisiteur = async (fields) => {
+        let results = {
+            error: false,
+            status: 200,
+            data: null
+        }
+
+        let visiteur = null
+
+        if(Object.values(fields).some(fieldsItem => fieldsItem === undefined || fieldsItem === null || fieldsItem === "")){
+            results.error = {
+                code: 400,
+                message: "BAD REQUEST - one param is null"
+            }
+            results.status = 400
+        } else {
+            try {
+                visiteur = await Visiteur.create(fields)
+    
+                if(visiteur){
+                    results.data = visiteur
+                    results.status = 201
+                }
+            } catch (CreateVisiteurError) {
+                console.error({CreateVisiteurError})
+                results.error = {
+                    code: 502,
+                    message: "BAD GATEWAY - error on creating ressource"
+                }
+                results.status = 502
+            }
+        }
+        return results
+    }
+
+    Visiteur.updateVisiteur = async (visiteurId, visiteurInfo) => {
+        let results = {
+            error: false,
+            status: 200,
+            data: null
+        }
+
+        let visiteur = null
+        let nextVisiteur = {...visiteurInfo}
+                
+        // Remove undifined keys
+        Object.keys(nextVisiteur).forEach(key => (nextVisiteur[key] === null || nextVisiteur[key] === "" || nextVisiteur[key] === undefined) && delete nextVisiteur[key])
+        
+        try {
+            [visiteurModifiedCount,] = await Visiteur.update(nextVisiteur, {
+                where: {
+                    id: visiteurId
+                }
+            })
+
+            if(visiteurModifiedCount === 1){
+                results.data = await Visiteur.findByPk(visiteurId)
+            } else {
+                results.error = {
+                    code: 404,
+                    message: "NOT FOUND - no visiteur found"
+                }
+                results.status = 404
+            }
+        } catch (UpdateVisiteurError) {
+            console.error({UpdateVisiteurError})
+            results.error = {
+                code: 502,
+                message: "BAD GATEWAY - error on updating ressource"
+            }
+            results.status = 502
+        }
+        
+        return results
+    }
+
     return Visiteur
 }
