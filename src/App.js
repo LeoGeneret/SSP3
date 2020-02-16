9d5c00c886b549e1969489d2a1c67c86a6b69c1e
Adrien Bannwarth <adrien.bann@gmail.com>
2020-02-16 14:23:21 +0100

    add routes

--- src/App.js
+++ src/App.js
@@ -6,17 +6,14 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from './pages/Home';
 import ListView from './pages/ListView';
 import Header from "./shared/Header";
-<<<<<<< HEAD
-import Planning from "./pages/Planning";
+// import Planning from "./pages/Planning";
 import Login from "./pages/Login";
 import ListVehicles from "./pages/ListVehicles";
 import ListHotels from "./pages/ListHotels"
 
-=======
 import Planning from "./pages/Planning"
 
 // connect api
->>>>>>> features/connect-api
 
 function App() {
   return (
