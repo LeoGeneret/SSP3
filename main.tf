547d21983a0a9c967d26c1b79e2bae2cf5fa41c8
KenyZ <zkeny@outlook.fr>
2020-07-06 12:34:50 +0200

    update docker + wip ansible

--- main.tf
+++ main.tf
@@ -66,38 +66,38 @@ resource "aws_instance" "rs_instance_ssp3" {
         host        = self.public_ip
     }
     
-    # Copy script that install docker and docker-compose 
-    provisioner "file" {
-      source      = "setup-docker.sh"
-      destination = "/tmp/setup-docker.sh"
-    }
+    # # Copy script that install docker and docker-compose 
+    # provisioner "file" {
+    #   source      = "setup-docker.sh"
+    #   destination = "/tmp/setup-docker.sh"
+    # }
 
-    # Write public ip in a file
-    provisioner "remote-exec" {
-      inline = [
-        "sudo bash -c 'echo MACHINE_HOST=${self.public_ip} > /mypublicip'"
-      ]
-    }
+    # # Write public ip in a file
+    # provisioner "remote-exec" {
+    #   inline = [
+    #     "sudo bash -c 'echo MACHINE_HOST=${self.public_ip} > /mypublicip'"
+    #   ]
+    # }
 
-    # Run script to install docker, docker-compose
-    provisioner "remote-exec" {
-      inline = [
-        "chmod +x /tmp/setup-docker.sh",
-        "/tmp/setup-docker.sh > /tmp/setup-docker-output",
-      ]
-    }
+    # # Run script to install docker, docker-compose
+    # provisioner "remote-exec" {
+    #   inline = [
+    #     "chmod +x /tmp/setup-docker.sh",
+    #     "/tmp/setup-docker.sh > /tmp/setup-docker-output",
+    #   ]
+    # }
     
-    # Copy script that launch the SSP3
-    provisioner "file" {
-      source      = "setup-ssp3-app.sh"
-      destination = "/tmp/setup-ssp3-app.sh"
-    }
+    # # Copy script that launch the SSP3
+    # provisioner "file" {
+    #   source      = "setup-ssp3-app.sh"
+    #   destination = "/tmp/setup-ssp3-app.sh"
+    # }
 
-    # Run script to launch SSP3
-    provisioner "remote-exec" {
-      inline = [
-        "chmod +x /tmp/setup-ssp3-app.sh",
-        "/tmp/setup-ssp3-app.sh > /tmp/setup-ssp3-app-output",
-      ]
-    }
+    # # Run script to launch SSP3
+    # provisioner "remote-exec" {
+    #   inline = [
+    #     "chmod +x /tmp/setup-ssp3-app.sh",
+    #     "/tmp/setup-ssp3-app.sh > /tmp/setup-ssp3-app-output",
+    #   ]
+    # }
 }
