f7e715dbc52e5cd731aee5afd00a4a7cc71fd5b6
KenyZ <zkeny@outlook.fr>
2020-07-06 13:35:30 +0200

    ansible setup servers done

--- main.tf
+++ main.tf
@@ -65,39 +65,4 @@ resource "aws_instance" "rs_instance_ssp3" {
         private_key = file(var.private_key_path)
         host        = self.public_ip
     }
-    
-    # # Copy script that install docker and docker-compose 
-    # provisioner "file" {
-    #   source      = "setup-docker.sh"
-    #   destination = "/tmp/setup-docker.sh"
-    # }
-
-    # # Write public ip in a file
-    # provisioner "remote-exec" {
-    #   inline = [
-    #     "sudo bash -c 'echo MACHINE_HOST=${self.public_ip} > /mypublicip'"
-    #   ]
-    # }
-
-    # # Run script to install docker, docker-compose
-    # provisioner "remote-exec" {
-    #   inline = [
-    #     "chmod +x /tmp/setup-docker.sh",
-    #     "/tmp/setup-docker.sh > /tmp/setup-docker-output",
-    #   ]
-    # }
-    
-    # # Copy script that launch the SSP3
-    # provisioner "file" {
-    #   source      = "setup-ssp3-app.sh"
-    #   destination = "/tmp/setup-ssp3-app.sh"
-    # }
-
-    # # Run script to launch SSP3
-    # provisioner "remote-exec" {
-    #   inline = [
-    #     "chmod +x /tmp/setup-ssp3-app.sh",
-    #     "/tmp/setup-ssp3-app.sh > /tmp/setup-ssp3-app-output",
-    #   ]
-    # }
 }
